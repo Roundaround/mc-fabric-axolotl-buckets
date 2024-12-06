41f486dd1525586ea71adbca135377f190a5ec9b
Roundaround <roundaroundna@gmail.com>
2024-12-06 13:35:59 -0500

    Removed stuff about dynamic models

--- src/main/java/me/roundaround/axolotlbuckets/client/AxolotlBucketUnbakedWrapperModel.java
+++
@@ -1,7 +0,0 @@
-package me.roundaround.axolotlbuckets.client;
-
-import net.fabricmc.fabric.api.client.model.loading.v1.WrapperUnbakedModel;
-import net.minecraft.client.render.item.model.ConditionItemModel;
-
-public class AxolotlBucketUnbakedWrapperModel extends WrapperUnbakedModel {
-}
--- src/main/java/me/roundaround/axolotlbuckets/client/AxolotlBucketsClientMod.java
+++ src/main/java/me/roundaround/axolotlbuckets/client/AxolotlBucketsClientMod.java
@@ -2,13 +2,8 @@ package me.roundaround.axolotlbuckets.client;
 
 import me.roundaround.roundalib.util.BuiltinResourcePack;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
-import net.fabricmc.fabric.api.client.model.loading.v1.ModelModifier;
-import net.fabricmc.fabric.api.client.model.loading.v1.WrapperUnbakedModel;
-import net.minecraft.client.data.ModelIds;
 import net.minecraft.client.render.item.property.bool.BooleanProperties;
 import net.minecraft.client.render.item.property.select.SelectProperties;
-import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -22,20 +17,5 @@ public final class AxolotlBucketsClientMod implements ClientModInitializer {
     BooleanProperties.ID_MAPPER.put(Identifier.of(MOD_ID, "baby"), BabyProperty.CODEC);
 
     BuiltinResourcePack.register(MOD_ID, RESOURCE_PACK_ID, Text.translatable("axolotlbuckets.resource.smaller"));
-
-    ModelLoadingPlugin.register((pluginContext) -> {
-      pluginContext.modifyModelOnLoad().register(ModelModifier.DEFAULT_PHASE, (model, context) -> {
-        if (!context.id().toString().equals(ModelIds.getItemModelId(Items.AXOLOTL_BUCKET).toString())) {
-          return model;
-        }
-
-        return new WrapperUnbakedModel(model) {
-          @Override
-          public void resolve(Resolver resolver) {
-            super.resolve(resolver);
-          }
-        };
-      });
-    });
   }
 }
